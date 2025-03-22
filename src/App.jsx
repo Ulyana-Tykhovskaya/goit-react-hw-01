67ca4fed209103a6717ef2f39a377759d0f829ac
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-03-22 13:45:56 +0100

    update-3dz

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Profile from "./components/Profile/Profile";
 import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import userData from "./userData.json";
 import friends from "./friends.json";
+import transactions from "./transactions.json";
 export default function App() {
   return (
     <div>
@@ -13,6 +15,7 @@ export default function App() {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 }
